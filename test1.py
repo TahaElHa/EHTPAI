56f9143f2304c4977c1c51a5239447e13601eb85
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 15:24:30 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,8 @@
 import streamlit as st
-st.write("Hello")
-st.title("Ceci est le titre de l'application")
-st.header("Ceci est l'en-tête")
-st.markdown("Ceci est la réduction")
-st.subheader("Ceci est le sous-en-tête")
-st.caption("Ceci est la légende")
-st.code("x = 2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.title("EMC2 / Cloud Module")
+st.header("Video test")
+st.video("https://www.youtube.com/watch?v=WJYV7Rm0OwU&ab_channel=%D9%8A%D9%84%D8%A7")
 
 
-st.sidebar.image("LogoEHTP.jpg")
+st.sidebar.image("ehtp.png")
+st.sidebar.header("Master Cloud Computing")
