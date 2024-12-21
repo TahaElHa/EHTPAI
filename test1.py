1e281257ac6c0fdd03cc1f76bf392931ffff84e9
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 15:30:57 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,4 +6,4 @@ st.video("https://www.youtube.com/watch?v=NPEsD6n9A_I&list=PLGjZwEtPN7j-Q59JYso3
 
 st.sidebar.image("ehtp.png")
 st.sidebar.header("Master Cloud Computing")
-st.sidebar.st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
+st.sidebar.selectbox('Select app type', ['----- Choose application -----', 'OCR', 'Image Analysis', 'Face Analysis', 'Thumbnail Image']) 
