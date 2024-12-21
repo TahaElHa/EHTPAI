fa971eb51d7ec37056f36d12fbbf8212a9ab9b27
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 15:38:20 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("EMC2 / Cloud Module")
-st.header("Video test")
+st.header("AI is Great")
 #st.video("https://www.youtube.com/watch?v=NPEsD6n9A_I&list=PLGjZwEtPN7j-Q59JYso3L4_yoCjj2syrM&ab_channel=AdamMarczak-AzureforEveryone")
 
 
