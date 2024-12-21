bdad36a48a70c449fcd5b1740d64dbcaae1f2131
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 16:33:28 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -11,3 +11,27 @@ choice = st.sidebar.selectbox('Select app type', ['----- Choose application ----
 
 if choice == 'Image Analysis':
   image_file = st.file_uploader('Upload an image', type = ['png', 'jpg'])
+
+
+# Use Read API to read text in image
+with open(image_file, mode="rb") as image_data:
+    read_op = cv_client.read_in_stream(image_data, raw=True)
+
+    # Get the async operation ID so we can check for the results
+    operation_location = read_op.headers["Operation-Location"]
+    operation_id = operation_location.split("/")[-1]
+
+    # Wait for the asynchronous operation to complete
+    while True:
+        read_results = cv_client.get_read_result(operation_id)
+        if read_results.status not in [OperationStatusCodes.running, OperationStatusCodes.not_started]:
+            break
+        time.sleep(1)
+
+    # If the operation was successfully, process the text line by line
+    if read_results.status == OperationStatusCodes.succeeded:
+        for page in read_results.analyze_result.read_results:
+            for line in page.lines:
+                print(line.text)
+                # Uncomment the following line if you'd like to see the bounding box 
+                #print(line.bounding_box)
