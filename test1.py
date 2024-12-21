792b48b0cef94e05f1d13a401821ee9cf30e0c13
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 15:16:10 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.write("Hello")
+st.title("Ceci est le titre de l'application")
+st.header("Ceci est l'en-tête")
+st.markdown("Ceci est la réduction")
+st.subheader("Ceci est le sous-en-tête")
+st.caption("Ceci est la légende")
+st.code("x = 2021")
+st.latex(r" a+a r^1+a r^2+a r^3 "")
