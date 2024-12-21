4c30e05ef285ae1d3318cd6ee09576ef91b8d6bd
TahaElHa <125458043+TahaElHa@users.noreply.github.com>
2024-12-21 15:36:48 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,13 @@
 import streamlit as st
 st.title("EMC2 / Cloud Module")
 st.header("Video test")
-st.video("https://www.youtube.com/watch?v=NPEsD6n9A_I&list=PLGjZwEtPN7j-Q59JYso3L4_yoCjj2syrM&ab_channel=AdamMarczak-AzureforEveryone")
+#st.video("https://www.youtube.com/watch?v=NPEsD6n9A_I&list=PLGjZwEtPN7j-Q59JYso3L4_yoCjj2syrM&ab_channel=AdamMarczak-AzureforEveryone")
 
 
 st.sidebar.image("ehtp.png")
 st.sidebar.header("Master Cloud Computing")
-st.sidebar.selectbox('Select app type', ['----- Choose application -----', 'OCR', 'Image Analysis', 'Face Analysis', 'Thumbnail Image']) 
+choice = st.sidebar.selectbox('Select app type', ['----- Choose application -----', 'OCR', 'Image Analysis', 'Face Analysis', 'Thumbnail Image']) 
+
+
+if choice == 'Image Analysis':
+  image_file = st.file_uploader('Upload an image', type = ['png', 'jpg'])
